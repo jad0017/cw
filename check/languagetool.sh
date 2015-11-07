6d6a3a540c33a93cdca082e522f4f44128f795c7
jad0017 <jad0017@uah.edu>
2015-11-07 09:05:47 -0600

    Minor updates Check framework

---
+++ check/languagetool.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar ~/LanguageTool-3.1/languagetool-commandline.jar $@
---
+++ check/textlint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+~/textlint/textlint.bash $@ ~/textlint/Linux32/pharo ~/textlint/Linux32/TextLint.image
